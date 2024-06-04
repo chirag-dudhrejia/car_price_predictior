e791453f4fcee5c148ef47a69a94cff74236db1f
Chir@g159 <dchirag159@gmail.com>
2024-06-04 17:06:46 +0530

    prediction page improvement

--- app.py
+++ app.py
@@ -10,17 +10,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    car_names = sorted(car_data["Car_name"].unique())
-    years = sorted(car_data["Year"].unique())
-    distance = sorted(car_data["Distance"].unique())
-    fuel_type = car_data["Fuel_type"].unique()
-    drive = car_data["Drive"].unique()
+    # car_names = sorted(car_data["Car_name"].unique())
+    # years = sorted(car_data["Year"].unique())
+    # distance = sorted(car_data["Distance"].unique())
+    # fuel_type = car_data["Fuel_type"].unique()
+    # drive = car_data["Drive"].unique()
+    all_data_list = get_all_data()
+    print(all_data_list[0])
     return render_template("index.html",
-                           car_names=car_names,
-                           years=years,
-                           distance=distance,
-                           fuel_type=fuel_type,
-                           drive=drive)
+                           car_names=all_data_list[0],
+                           years=all_data_list[1],
+                           distance=all_data_list[2],
+                           fuel_type=all_data_list[3],
+                           drive=all_data_list[4])
 
 
 @app.route("/performing_prediction", methods=["POST"])
@@ -32,11 +34,27 @@ def perform_prediction():
     fuel = values["fuel_type"]
     drive = values["drive"]
     kms_driven = values["km_travelled"]
-    # print(company, car_model, year, fuel, kms_driven)
+    all_data_list = get_all_data()
+
     prediction = model.predict(pd.DataFrame(data=[[car_model, year, kms_driven, fuel, drive]],
                                             columns=["Car_name", "Year", "Distance", "Fuel_type", "Drive"]))
     prediction = round(prediction[0])
-    return render_template("index.html", prediction=prediction)
+    return render_template("index.html",
+                           prediction=prediction,
+                           car_names=all_data_list[0],
+                           years=all_data_list[1],
+                           distance=all_data_list[2],
+                           fuel_type=all_data_list[3],
+                           drive=all_data_list[4])
+
+
+def get_all_data():
+    car_names = sorted(car_data["Car_name"].unique())
+    years = sorted(car_data["Year"].unique())
+    distance = sorted(car_data["Distance"].unique())
+    fuel_type = car_data["Fuel_type"].unique()
+    drive = car_data["Drive"].unique()
+    return [car_names, years, distance, fuel_type, drive]
 
 
 if __name__ == "__main__":
